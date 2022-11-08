8c58e12a613f02257ce1fcf29d00783005e1ecfa
Davi Menezes <davimenezes.dev@gmail.com>
2022-11-08 14:09:58 -0300

    seeders and refactorings

--- Models/WorkspaceModel.php
+++ Models/WorkspaceModel.php
@@ -2,7 +2,9 @@
 
 namespace Modules\Workspace\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Modules\Base\Models\BaseModel;
 use Modules\Workspace\Database\Factories\WorkspaceFactory;
 use Modules\Workspace\Entities\Workspace\WorkspaceEntityModel;
@@ -11,6 +13,7 @@ use Modules\Workspace\Entities\Workspace\WorkspaceProps;
 /**
  * @author Davi Menezes (davimenezes.dev@gmail.com)
  * @link https://github.com/DaviMenezes
+ * @property-read User $user
  * @method WorkspaceEntityModel toEntity()
  * @method static WorkspaceFactory factory()
  */
@@ -32,5 +35,10 @@ class WorkspaceModel extends BaseModel
     public static function table($alias = null): string
     {
         return self::dbTable('workspaces', $alias);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
