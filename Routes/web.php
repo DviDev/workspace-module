ea053b85a33ea21181d9178e451dc0faa21d0796
Davi Menezes <davimenezes.dev@gmail.com>
2023-04-21 19:52:37 -0300

    workspace routes moved to module

--- Routes/web.php
+++ Routes/web.php
@@ -10,3 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Modules\Workspace\Models\WorkspaceModel;
+
+Route::prefix('workspace')->group(function () {
+    Route::view('/list', 'workspace::components.page.workspaces')->name('admin.workspaces');
+    Route::get('/{workspace}/edit', fn(WorkspaceModel $workspace) =>
+    view('workspace::components.page.workspace_edit', compact('workspace')))
+        ->name('admin.workspace.edit');
+    Route::get('/{workspace}/chats', fn(WorkspaceModel $w) =>
+    view('workspace::components.page.workspace_chats_page', compact('w')))
+        ->name('admin.workspace.chats');
+    Route::get('/{workspace}/links', fn(WorkspaceModel $w) =>
+        view('workspace::components.page.workspace_links_page', compact('w')))
+            ->name('admin.workspace.links');
+    Route::get('/{workspace}/participants', fn(WorkspaceModel $w) =>
+        view('workspace::components.page.workspace_participants_page', compact('w')))
+            ->name('admin.workspace.participants');
+    Route::get('/{workspace}/posts', fn(WorkspaceModel $w) =>
+        view('workspace::components.page.workspace_posts_page'))
+        ->name('admin.workspace.posts');
+    Route::get('/{workspace}/projects', fn(WorkspaceModel $w) =>
+        view('workspace::components.page.workspace_projects_page', compact('w')))
+            ->name('admin.workspace.projects');
+    Route::get('/{workspace}/tags', fn(WorkspaceModel $w) =>
+        view('workspace::components.page.workspace_tags_page', compact('w')))
+            ->name('admin.workspace.tags');
+});
