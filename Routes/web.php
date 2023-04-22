4180ec061e030200f0b66f5d160300ec3080e70e
Davi Menezes <davimenezes.dev@gmail.com>
2023-04-21 21:42:54 -0300

    fix routes

--- Routes/web.php
+++ Routes/web.php
@@ -18,22 +18,22 @@ Route::prefix('workspace')->group(function () {
     Route::get('/{workspace}/edit', fn(WorkspaceModel $workspace) =>
     view('workspace::components.page.workspace_edit', compact('workspace')))
         ->name('admin.workspace.edit');
-    Route::get('/{workspace}/chats', fn(WorkspaceModel $w) =>
-    view('workspace::components.page.workspace_chats_page', compact('w')))
+    Route::get('/{workspace}/chats', fn(WorkspaceModel $workspace) =>
+    view('workspace::components.page.workspace_chats_page', compact('workspace')))
         ->name('admin.workspace.chats');
-    Route::get('/{workspace}/links', fn(WorkspaceModel $w) =>
-        view('workspace::components.page.workspace_links_page', compact('w')))
+    Route::get('/{workspace}/links', fn(WorkspaceModel $workspace) =>
+        view('workspace::components.page.workspace_links_page', compact('workspace')))
             ->name('admin.workspace.links');
-    Route::get('/{workspace}/participants', fn(WorkspaceModel $w) =>
-        view('workspace::components.page.workspace_participants_page', compact('w')))
+    Route::get('/{workspace}/participants', fn(WorkspaceModel $workspace) =>
+        view('workspace::components.page.workspace_participants_page', compact('workspace')))
             ->name('admin.workspace.participants');
-    Route::get('/{workspace}/posts', fn(WorkspaceModel $w) =>
-        view('workspace::components.page.workspace_posts_page'))
+    Route::get('/{workspace}/posts', fn(WorkspaceModel $workspace) =>
+        view('workspace::components.page.workspace_posts_page', compact('workspace')))
         ->name('admin.workspace.posts');
-    Route::get('/{workspace}/projects', fn(WorkspaceModel $w) =>
-        view('workspace::components.page.workspace_projects_page', compact('w')))
+    Route::get('/{workspace}/projects', fn(WorkspaceModel $workspace) =>
+        view('workspace::components.page.workspace_projects_page', compact('workspace')))
             ->name('admin.workspace.projects');
-    Route::get('/{workspace}/tags', fn(WorkspaceModel $w) =>
-        view('workspace::components.page.workspace_tags_page', compact('w')))
+    Route::get('/{workspace}/tags', fn(WorkspaceModel $workspace) =>
+        view('workspace::components.page.workspace_tags_page', compact('workspace')))
             ->name('admin.workspace.tags');
 });
