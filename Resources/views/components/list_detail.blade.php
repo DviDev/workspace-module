a9ad821ec01228c3b8da5322247c8cc52ccaf8fb
Davi Menezes <davimenezes.dev@gmail.com>
2023-04-22 22:11:31 -0300

    improvements

--- Resources/views/components/list_detail.blade.php
+++ Resources/views/components/list_detail.blade.php
@@ -1,17 +1,12 @@
-<div class="dark:text-gray-200 text-gray-800 px-4 space-y-1">
-    <div class="space-x-1">
-        <x-button href="{{route('admin.workspace.projects', $row->id)}}" label="projetos" teal/>
-
-        <x-button href="{{route('admin.workspace.posts', $row->id)}}" label="posts" teal/>
-
-        <x-button href="{{route('admin.workspace.links', $row->id)}}" label="links" teal/>
-
-        <x-button href="{{route('admin.workspace.participants', $row->id)}}" label="participants" teal/>
-
-        <x-button href="{{route('admin.workspace.chats', $row->id)}}" label="chats" teal/>
-        <x-button href="{{route('admin.social.groups', $row->id)}}" label="social" teal/>
-
-        <x-button href="{{route('admin.workspace.tags', $row->id)}}" label="tags" teal/>
-    </div>
+<div class="dark:text-gray-200 px-4 space-y-1">
+    <x-dvui::button.group class="text-sm border dark:border-gray-500 dark:text-gray-400 divide-x divide-gray-500">
+        <x-dvui::link text="projects" :url="route('admin.workspace.projects', $row->id)" white class=" rounded-l-md px-2 pl-3 py-1"/>
+        <x-dvui::link text="posts" :url="route('admin.workspace.posts', $row->id)" white class=" px-2 py-1"/>
+        <x-dvui::link text="links" :url="route('admin.workspace.links', $row->id)" white class="px-2 py-1"/>
+        <x-dvui::link text="participants" :url="route('admin.workspace.participants', $row->id)" white class="px-2 py-1"/>
+        <x-dvui::link text="chats" :url="route('admin.workspace.chats', $row->id)" white class="px-2 py-1"/>
+        <x-dvui::link text="social" :url="route('admin.social.groups', $row->id)" white class="px-2 py-1"/>
+        <x-dvui::link text="tags" :url="route('admin.workspace.tags', $row->id)" white class="rounded-r-md px-2 py-1 pr-4"/>
+    </x-dvui::button.group>
     <div>Descrição: {{$row->description}}</div>
 </div>
--- Routes/web.php
+++ Routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
 use Modules\Workspace\Models\WorkspaceModel;
 
 Route::prefix('workspace')->group(function () {
