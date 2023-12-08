bc92d9a674bd7a2da70203ccc9065b1aa5bc4ec4
Davi Menezes <davimenezes.dev@gmail.com>
2023-12-08 19:07:59 -0300

    improvements

--- Resources/views/components/page/workspace_edit.blade.php
+++ Resources/views/components/page/workspace_edit.blade.php
@@ -1,3 +1,3 @@
-<x-app-layout>
-    editing ...
-</x-app-layout>
+<x-lte::layout.v1.page title="Workspace">
+    <livewire:workspace::form :model="$workspace" :page="\Modules\View\Models\ModuleEntityPageModel::find(1)"/>
+</x-lte::layout.v1.page>
