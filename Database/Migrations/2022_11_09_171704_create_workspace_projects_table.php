0bdf9ee0bfcaff7e0a59ba0d48002bf869797ce7
Davi Menezes <davimenezes.dev@gmail.com>
2024-02-19 04:10:18 -0300

    refactorings

--- Database/Migrations/2022_11_09_171704_create_workspace_projects_table.php
+++ Database/Migrations/2022_11_09_171704_create_workspace_projects_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Modules\Workspace\Entities\WorkspaceProject\WorkspaceProjectEntityModel;
+use Nwidart\Modules\Facades\Module;
 
 return new class extends Migration
 {
@@ -14,6 +15,9 @@ return new class extends Migration
      */
     public function up()
     {
+        if (!collect(Module::allEnabled())->contains('Project')) {
+            return;
+        }
         Schema::create('workspace_projects', function (Blueprint $table) {
             $p = WorkspaceProjectEntityModel::props(null, true);
             $table->id();
