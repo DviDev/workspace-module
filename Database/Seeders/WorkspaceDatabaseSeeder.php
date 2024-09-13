6bef40326e04e2fae9ef1996593270165ecba1fc
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-13 18:35:12 -0300

    seeders, listeners, providers

--- Database/Seeders/WorkspaceDatabaseSeeder.php
+++ Database/Seeders/WorkspaceDatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Modules\Workspace\Database\Seeders;
 
 use Illuminate\Database\Eloquent\Model;
 use Modules\Base\Database\Seeders\BaseSeeder;
-use Modules\Project\Database\Seeders\ProjectTableSeeder;
-use Nwidart\Modules\Facades\Module;
 
 class WorkspaceDatabaseSeeder extends BaseSeeder
 {
@@ -19,21 +17,7 @@ class WorkspaceDatabaseSeeder extends BaseSeeder
         Model::unguard();
 
         $this->commandWarn(__CLASS__, "🌱 seeding");
-
-        $modules = collect(Module::allEnabled());
-        if ($modules->contains('DBMap')) {
-
-
-            if ($modules->contains('Project')) {
-//                $module = ProjectModuleModel::byName('Workspace');
-                $this->call(ProjectTableSeeder::class, parameters: [
-                    'module_name' => 'DBMap',
-//                    'project' => $module->project,
-                ]);
-            }
-        }
-
-
+        //...
         $this->commandInfo(__CLASS__, '🟢 done');
     }
 }
