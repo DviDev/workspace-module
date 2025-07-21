9b2eb3cb16b9f2fff4f2db7d956c610bac4d3453
Davi Menezes <davimenezes.dev@gmail.com>
2025-07-21 14:40:47 -0300

    refactorings

--- Resources/views/components/page/workspaces.blade.php
+++ Resources/views/components/page/workspaces.blade.php
@@ -1,32 +1,28 @@
-<x-lte::layout.v1.page title="Workspaces">
-    <x-lte::card.body>
-        <table class="table table-sm table-hover">
-            <thead>
+@php use Modules\Workspace\Models\WorkspaceModel; @endphp
+<x-dvui::layouts.page title="Workspaces">
+    <table class="table table-sm table-hover">
+        <thead>
+        <tr>
+            <th style="width: 5%">Id</th>
+            <th>Nome</th>
+            <th style="width: 10%">#</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($workspaces = WorkspaceModel::paginate() as $workspace)
             <tr>
-                <th style="width: 5%">Id</th>
-                <th>Nome</th>
-                <th style="width: 10%">#</th>
+                <td>{{$workspace->id}}</td>
+                <td>{{$workspace->name}}</td>
+                <td>
+                    <div>
+                        <a href="{{route('workspace.form', $workspace->id)}}"
+                           class="bg-green-600 px-2 py-1 rounded text-white">
+                            <i class="fas fa-pencil-alt"></i>
+                        </a>
+                    </div>
+                </td>
             </tr>
-            </thead>
-            <tbody>
-            @foreach($workspaces = \Modules\Workspace\Models\WorkspaceModel::paginate() as $workspace)
-                <tr>
-                    <td>{{$workspace->id}}</td>
-                    <td>{{$workspace->name}}</td>
-                    <td>
-                        <div>
-                            <a href="{{route('workspace.form', $workspace->id)}}"
-                               class="bg-green-600 px-2 py-1 rounded text-white">
-                                <i class="fas fa-pencil-alt"></i>
-                            </a>
-                        </div>
-                    </td>
-                </tr>
-
-            @endforeach
-            </tbody>
-        </table>
-    </x-lte::card.body>
-
-    {{--    <livewire:workspace.workspace-table/>--}}
-</x-lte::layout.v1.page>
+        @endforeach
+        </tbody>
+    </table>
+</x-dvui::layouts.page>
