822b14c2fbe1037da293c3a5b6d95cf454188d8d
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-23 15:53:36 -0300

    Add Portuguese translations for workspace modules
    - Introduced new translation files for workspace chats, links, participants, posts, and projects in `pt_BR`.

---
+++ lang/pt_BR/workspace_participants.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'id' => 'id',
+    'workspace_id' => 'workspace',
+    'user_id' => 'usuário',
+    'created_at' => 'data de criação',
+    'deleted_at' => 'data de remoção'
+];
