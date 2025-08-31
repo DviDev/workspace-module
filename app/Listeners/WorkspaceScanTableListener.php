5150410987c0a1895dfa599677c341bf25afcdbf
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-30 23:28:59 -0300

    Implement queued listeners across modules
    Added `ShouldQueue` to multiple listeners to enable background execution and improve performance. Simplified logic for some listeners by delegating tasks to jobs or helper services.

--- app/Listeners/WorkspaceScanTableListener.php
+++ app/Listeners/WorkspaceScanTableListener.php
@@ -2,17 +2,18 @@
 
 namespace Modules\Workspace\Listeners;
 
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Modules\DBMap\Domains\ScanTableDomain;
 use Modules\DBMap\Events\ScanTableEvent;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
-class WorkspaceScanTableListener
+class WorkspaceScanTableListener implements ShouldQueue
 {
     public function handle(ScanTableEvent $event): void
     {
         $output = new ConsoleOutput;
         $output->writeln(PHP_EOL.'🤖  Workspace module: scanning ...');
-        (new ScanTableDomain)->scan('workspace');
+        new ScanTableDomain()->scan('workspace');
         $output->writeln('🤖  Workspace module: ✔ DONE');
     }
 }
