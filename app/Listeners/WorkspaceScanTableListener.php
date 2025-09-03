eaa6256b9426fc3c2f1077698e228e94085cf9cf
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-03 02:43:12 -0300

    Remove console output from `WorkspaceScanTableListener` for cleaner execution.

--- app/Listeners/WorkspaceScanTableListener.php
+++ app/Listeners/WorkspaceScanTableListener.php
@@ -5,15 +5,11 @@ namespace Modules\Workspace\Listeners;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Modules\DBMap\Domains\ScanTableDomain;
 use Modules\DBMap\Events\ScanTableEvent;
-use Symfony\Component\Console\Output\ConsoleOutput;
 
 class WorkspaceScanTableListener implements ShouldQueue
 {
     public function handle(ScanTableEvent $event): void
     {
-        $output = new ConsoleOutput;
-        $output->writeln(PHP_EOL.'🤖  Workspace module: scanning ...');
         new ScanTableDomain()->scan('workspace');
-        $output->writeln('🤖  Workspace module: ✔ DONE');
     }
 }
