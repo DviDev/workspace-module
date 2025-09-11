0e5cda1042cd095c5e320a134b4ce3b4349eb47b
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-10 21:06:47 -0300

    "Remove unused Workspace views and associated routes for workspace pages to streamline codebase"

--- resources/views/components/page/workspace_projects_page.blade.php
+++
@@ -1,5 +0,0 @@
-<x-dvui::layouts.page>
-    <div class="dark:text-gray-200 text-gray-800 my-auto flex flex-col justify-center items-center">
-        {{__('content')}}
-    </div>
-</x-dvui::layouts.page>
